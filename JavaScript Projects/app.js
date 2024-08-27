21547334fccf79466cbaad524852621a92def18c
Nebil A. <nebilabdulfetahbeshir@gmail.com>
2024-08-27 10:11:05 +0300

    Organizing functions in javascript and adding mobile responsiveness

--- JavaScript Projects/app.js
+++ JavaScript Projects/app.js
@@ -1,31 +1,28 @@
-//  1. select the form element and get it's text value
-//  2. create a div and use the value to create a div
-//  3. add a delete button to the new div
-//  4. append that div in the todo list container div
-//  5. make that item dissapear if the delete button is clicked
 const addTask = document.getElementById("add-task");
 const listContainer = document.getElementById("list-container");
 //selecting the form submit button and list container
 
-addTask.addEventListener("click", () => {
-    const task = document.getElementById("task") //input value
-    // const taskTest = task.value;
+const task = document.getElementById("task") //input value
 
-    let newTask = document.createElement("div");
-    newTask.innerHTML = `<p> ${task.value} </p>`;
+
+addTask.addEventListener("click", createTask);
+
+
+function createTask() {
+    let newTask = document.createElement("div"); 
     let deleteButton = document.createElement("button");
     deleteButton.innerHTML = "DELETE";
-    newTask.append(deleteButton); //adding thr delete button
-    // creating the task element with its div inside it a paragraph and button
-    
-    listContainer.append(newTask); //add the full task items inside the container
-    
-    task.value = ""; //clearing the form
+    newTask.innerHTML = `<p> ${task.value} </p>`; 
+    newTask.append(deleteButton)            // creating the task element with its div inside it a paragraph and button
+    listContainer.append(newTask);           //add the full task items inside the container
+    task.value = "";                        //clearing the form
+   
+    //deleting functionality
     deleteButton.addEventListener("click", ()=>{ //checking when the delete task button is clicked
-        while (newTask.hasChildNodes()) { 
-            newTask.removeChild(newTask.firstChild);
-          } //removing ever element of that task
-          newTask.style.display = "none"; //displaying as none to avoid any glitches
-    })
-})
-
+            while (newTask.hasChildNodes()) { 
+                newTask.removeChild(newTask.firstChild);
+        
+              }
+              newTask.style.display = "none"; //displaying as none to avoid any glitches
+        })
+}
